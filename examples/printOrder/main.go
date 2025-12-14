5af39e45b539480f00002687c4cea8579e07f5d1
Kyle Weddermann <themilkman311@gmail.com>
2025-12-14 13:59:50 -0500

    typo

--- examples/printOrder/main.go
+++ examples/printOrder/main.go
@@ -28,7 +28,7 @@ func main() {
 	// No comment, empty slice
 	enum.Foreach(func(value byte) {
 		fmt.Println(value)
-	}, "cons", "Print fast.")
+	}, "con", "Print fast.")
 
 	// Notice the index for this Map will be 1 since it's the second time Map is called.
 	enum.Map(func(value byte) byte {
